76fc69e93f8173ade1577e462ad2ec6ddbb4a6de
Raunakk18 <raunakjha654@gmail.com>
2025-05-22 19:47:08 +0530

    Finished building menu section

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const menuOpenButton = document.querySelector("#menu-open-button");
-const menuCloseButton = document.querySelector("#menu-close-button");
+const menuOpenButton = document.querySelector("#menu-open-button");;
+const menuCloseButton = document.querySelector("#menu-close-button");;
 
-menuOpenButton.addEventListener("click", ()=>{
-    document.body.classList.toggle("show-mobile-menu");
+menuOpenButton.addEventListener("click", ()=>{;
+    document.body.classList.toggle("show-mobile-menu");;
 })
 
-menuCloseButton.addEventListener("click", ()=> menuOpenButton.click());
+menuCloseButton.addEventListener("click", ()=> menuOpenButton.click());;
