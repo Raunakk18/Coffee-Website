f2ff2e903782c0fd71d4bd7af9494e3deeaee7ac
Raunakk18 <raunakjha654@gmail.com>
2025-05-26 11:33:23 +0530

    finished building the complete coffee website

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("show-mobile-menu");
+    });
+});
 const menuOpenButton = document.querySelector("#menu-open-button");
-const menuCloseButton = document.querySelector("#menu-close-button");;
+const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", ()=>{;
-    document.body.classList.toggle("show-mobile-menu");;
+    document.body.classList.toggle("show-mobile-menu");
 })
 
 menuCloseButton.addEventListener("click", ()=> menuOpenButton.click());
